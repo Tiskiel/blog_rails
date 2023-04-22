6d5db2f0112c6461b2794feed6d7cc34c3a054eb
Wanty Jean-François <wanty.jf@gmail.com>
2023-04-22 09:22:21 +0200

    Create controller and page home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
   get 'hello_world', to: 'application#hello_world'
 end
